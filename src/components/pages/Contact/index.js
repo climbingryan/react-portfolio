a45c3d40634f7caeb615af2baa302a13ffb03208
shaggy <simmons5family@gmail.com>
2021-03-14 18:10:18 -0500

    add form functionality

--- src/components/pages/Contact/index.js
+++ src/components/pages/Contact/index.js
@@ -1,22 +1,82 @@
-import React from 'react'
+import { useState } from "react";
+import { validateEmail } from '../../../utils/helpers';
 
-const index = () => {
-    return (
-        <div>
-            <h1>Contact</h1>
-            <p>
-                Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis molestie urna.
-                Aliquam semper ultrices varius. Aliquam faucibus sit amet magna a ultrices. Aenean
-                pellentesque placerat lacus imperdiet efficitur. In felis nisl, luctus non ante euismod,
-                tincidunt bibendum mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-                posuere, eget tristique dui dapibus. Maecenas fermentum elementum faucibus. Quisque nec metus
-                vestibulum, egestas massa eu, sollicitudin ipsum. Nulla facilisi. Sed ut erat ligula. Nam
-                tincidunt nunc in nibh dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-                conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at rhoncus. Etiam vel
-                condimentum magna, quis tempor nulla.
-            </p>
+const Contact = (props) => {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const { name, email, message } = formState;
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (!errorMessage) {
+        setFormState({ [e.target.name]: e.target.value });
+        console.log('Form', formState);
+      }
+
+      setFormState('');
+  };
+
+  const handleChange = (e) => {
+    if (e.target.name === 'email') {
+        console.log(e.target.name);
+        const validEmail = validateEmail(e.target.value);
+        if(!validEmail) {
+            setErrorMessage('InvalidEmail')
+        } else {
+            setErrorMessage('')
+        }
+    } else {
+        if(!e.target.value.length) {
+            setErrorMessage(`${e.target.name} is required`)
+        } else {
+            setErrorMessage('')
+        }
+    }
+  }
+
+  return (
+    <div>
+      <h2 className="page-title">Contact</h2>
+      <form onSubmit={onSubmit}>
+        <div className="input-control">
+          <label>Name: </label>
+          <input
+            type="text"
+            placeholder="Your Name"
+            defaultValue={name}
+            name='name'
+            onBlur={handleChange}
+          ></input>
         </div>
-    )
-}
+        <div className="input-control">
+          <label>Email: </label>
+          <input
+            type="text"
+            placeholder="Your Email"
+            defaultValue={email}
+            name='email'
+            onBlur={handleChange}
+          ></input>
+        </div>
+        <div className="input-control">
+          <label>Message: </label>
+          <textarea
+            type="text-box"
+            defaultValue={message}
+            onBlur={handleChange}
+          ></textarea>
+        </div>
+        <input type="submit" value="Submit" className="btn"></input>
+        {errorMessage && <p className="form-error">{errorMessage}</p>}
+      </form>
+    </div>
+  );
+};
 
-export default index
+export default Contact;
